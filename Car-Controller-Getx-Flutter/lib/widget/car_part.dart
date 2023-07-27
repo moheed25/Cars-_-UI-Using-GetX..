5a676c0ba9c0c598a9f0f1dac6b1a908b6a2b917
moheed25 <mohedaziz123@gmail.com>
2023-07-27 16:48:50 +0500

    complete

---
+++ Car-Controller-Getx-Flutter/lib/widget/car_part.dart
@@ -0,0 +1,64 @@
+import 'package:car_controller/widget/text_widget.dart';
+import 'package:flutter/material.dart';
+
+class CarPart extends StatelessWidget {
+  final bool state;
+  final String name;
+  final VoidCallback onTap;
+  const CarPart(
+      {super.key,
+      required this.state,
+      required this.name,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 10),
+      height: 140,
+      width: 160,
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: state ? Colors.blue : Colors.transparent,
+              blurRadius: state ? 50 : 0,
+            )
+          ],
+          color: state ? Colors.blue : Colors.black.withOpacity(.5),
+          borderRadius: BorderRadius.circular(20)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              text(name, 20, Colors.white, FontWeight.bold),
+              const SizedBox(
+                height: 5,
+              ),
+              text(state ? "Opened" : "Closed", 20,
+                  Colors.white.withOpacity(.5), FontWeight.bold),
+            ],
+          ),
+          const Spacer(),
+          Transform.rotate(
+            angle: -1.6,
+            child: Transform.scale(
+              scaleX: 1.5,
+              scaleY: 1.5,
+              child: Switch(
+                activeColor: Colors.blue,
+                activeTrackColor: Colors.white,
+                inactiveTrackColor: Colors.white.withOpacity(.1),
+                value: state,
+                onChanged: (value) => onTap(),
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
